1b4dc2d9b7c4b1a541e65822f3030141c3efef0c
gabriel velez <gabriel.dan.velez@gmail.com>
2022-04-02 01:42:37 -0400

    .

--- src/components/PizzaFormSuccess.js
+++ src/components/PizzaFormSuccess.js
@@ -2,10 +2,10 @@ import React from "react";
 
 export default function PizzaFormSuccess() {
   return (
-    <>
+    <div>
       <h1>Congrats!</h1>
       <h1>The Pizza is on the way!</h1>
       <img src='https://i.giphy.com/media/9fuvOqZ8tbZOU/giphy.webp'></img>
-    </>
+    </div>
   );
 }
