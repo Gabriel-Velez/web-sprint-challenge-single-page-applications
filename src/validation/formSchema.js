d29feee61d2b0f3f7f6882d4f5753a21cff3a716
gabriel velez <gabriel.dan.velez@gmail.com>
2022-04-06 19:14:24 -0400

    .

--- src/validation/formSchema.js
+++ src/validation/formSchema.js
@@ -2,7 +2,7 @@ import * as yup from "yup";
 substitute: yup.object();
 
 const formSchema = yup.object().shape({
-  name: yup.string().required("Please enter a name").min(2, "Name must be more than 2 charachters"),
+  name: yup.string().required("Please enter a name").min(2, "name must be at least 2 characters"),
   size: yup.string().required("Please pick a size"),
   sauce: yup.string().required("Please select a sauce"),
   toppings: yup.number().max(10, "Please select only 10 toppings"),
